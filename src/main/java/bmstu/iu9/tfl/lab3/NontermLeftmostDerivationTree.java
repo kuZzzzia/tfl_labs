36713648997f594f52457967af44b872d8f2df8a
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2021-11-12 17:38:14 +0300

    Added leftmost derivation of a nonterm

--- src/main/java/bmstu/iu9/tfl/lab3/NontermLeftmostDerivationTree.java
+++ src/main/java/bmstu/iu9/tfl/lab3/NontermLeftmostDerivationTree.java
@@ -2,11 +2,55 @@ package bmstu.iu9.tfl.lab3;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class NontermLeftmostDerivationTree {
-    private TreeNode node;
+    private enum RegularStatus {
+        NOT_DETERMINED,
+        REGULAR,
+        PROBABLY_REGULAR,
+        SUSPECT
+    }
+
+    private final TreeNode node;
+    private RegularStatus status;
 
     public NontermLeftmostDerivationTree(String nonterm, Grammar rules) {
-        node = new TreeNode(nonterm, rules, nonterm, new HashSet<>());
+        status = RegularStatus.NOT_DETERMINED;
+        node = new TreeNode(nonterm, rules, nonterm, false, new HashSet<>());
+        if (!node.checkRootNontermFound()) {
+            status = RegularStatus.PROBABLY_REGULAR;
+        } else {
+            String prefix = node.buildLeftMostDerivationToRootNonterm(nonterm);
+            List<String> suffix = node.getNontermsAndTermsSuffixOfRootNontermDerivationTree();
+            if (suffix.isEmpty()) {
+                status = RegularStatus.PROBABLY_REGULAR;
+            } else {
+                if (checkRegularSubsetContainsSuffix && checkWordBelongsToSuffixIterLanguage()) {
+                    Set<StringBuilder> words = node.getWords();
+                    if (words.isEmpty()) {
+                        status = RegularStatus.SUSPECT;
+                        //leftmostDerivationTree may be not regular
+                    } else {
+                        for (StringBuilder word : words) {
+                            if (!checkWordBelongsToSuffixIterLangugage()) {
+                                status = RegularStatus.SUSPECT;
+                                //leftmostDerivationTree may be not regular
+                            }
+                        }
+                    }
+                    if (status == RegularStatus.NOT_DETERMINED) {
+                        status = (checkRegularSubsetContainsRootNonterm()) ? RegularStatus.REGULAR : RegularStatus.PROBABLY_REGULAR;
+                    }
+                } else {
+                    status = RegularStatus.SUSPECT;
+                }
+            }
+            //recursiveClosureOfAllNonterms that were created by NontermLeftmostDerivationTree
+            // if (all nonterms are regular) then it is regular
+            // if (all terms are regular and probably_regular) then it is probably_regular
+            // if (all terms are probably_regular) then it is probably_regular
+            // if it has all non-terms that are probably_regular then language regularity can not be determined
+        }
     }
 }
--- src/main/java/bmstu/iu9/tfl/lab3/TreeNode.java
+++ src/main/java/bmstu/iu9/tfl/lab3/TreeNode.java
@@ -5,51 +5,94 @@ import java.util.*;
 public class TreeNode {
     private static final int TERM_STARTING_INDEX = 0;
 
+    private final String         nodeExpr;
 
-    private boolean              isNonterm;
-    private String               nodeExpr;
+    private final boolean        isNonterm;
     private List<List<TreeNode>> middleNode;
     private List<List<String>>   rightNode;
     private Set<StringBuilder>   words;
 
+    private final boolean        isEndOfDerivation;
+    private boolean              rootNontermFound;
+    private int                  indexOfRootNontermInMiddleNodeList;
+    private List<String>         nontermsAndTermsSuffixOfRootNontermDerivationTree;
 
-    public TreeNode(String nonterm, Grammar rules, String rootNonterm, Set<String> stackNontermsTrace) {
+
+    public TreeNode(String nonterm, Grammar rules, String rootNonterm, boolean rootNontermFound, Set<String> stackNontermsTrace) {
         isNonterm = true;
         nodeExpr = nonterm;
         middleNode = new ArrayList<>();
         rightNode = new ArrayList<>();
         words = new HashSet<>();
+        isEndOfDerivation = false;
+        this.rootNontermFound = rootNontermFound;
+
         List<String[]> rewritingVariants = rules.getNontermRewritingVariants(nonterm);
         for (String[] rewritingVariant : rewritingVariants) {
             List<TreeNode> rewritingVariantMiddleNode = new ArrayList<>();
-            rewritingVariantMiddleNode.add(new TreeNode(rewritingVariant[TERM_STARTING_INDEX]));
+            rewritingVariantMiddleNode.add(new TreeNode(rewritingVariant[TERM_STARTING_INDEX], false));
+            middleNode.add(rewritingVariantMiddleNode);
             rightNode.add(new ArrayList<>(Arrays.asList(Arrays.copyOfRange(rewritingVariant, 1, rewritingVariant.length))));
         }
-        List<Integer> noRootNontermFoundAfterDerivation = new ArrayList<>();
-        for (int i = 0; i < rightNode.size(); i++) {
-            List<String> rightNodeVariant = rightNode.get(i);
-            if (rightNodeVariant.isEmpty()) {
-                Set<StringBuilder> derivationToTermsString = buildShortestDerivationToTermsString(middleNode.get(i));
-                if (derivationToTermsString != null) {
-                    words.addAll(derivationToTermsString);
-                }
-                noRootNontermFoundAfterDerivation.add(0, i);
-            } else {
-                String expr = rightNodeVariant.get(0);
-                if (expr.matches(RuleRightSide.NONTERM_REGEX)) {
-                    Set<String> newStackNontermsTrace = new HashSet<>(stackNontermsTrace);
-                    newStackNontermsTrace.add(expr);
-                    middleNode.get(i).add(new TreeNode(expr, rules, rootNonterm, newStackNontermsTrace));
-                } else {
-                    middleNode.get(i).add(new TreeNode(expr));
-                }
-            }
+
+        Set<Integer> endedDerivation = new HashSet<>();
+        while (endedDerivation.size() != middleNode.size()) {
+            iterLeftmostDerivation(rules, rootNonterm, stackNontermsTrace, endedDerivation);
         }
     }
 
-    public TreeNode(String termsString) {
-        isNonterm = false;
+    public TreeNode(String termsString, boolean isEndOfDerivation) {
+        isNonterm = isEndOfDerivation;
         nodeExpr = termsString;
+        this.isEndOfDerivation = isEndOfDerivation;
+    }
+
+    private void iterLeftmostDerivation(Grammar rules, String rootNonterm, Set<String> stackNontermsTrace, Set<Integer> endedDerivation) {
+        List<Integer> circledDerivation = new ArrayList<>();
+        for (int i = 0; i < rightNode.size(); i++) {
+            if (!rootNontermFound || i != indexOfRootNontermInMiddleNodeList) {
+                List<String> rightNodeVariant = rightNode.get(i);
+                if (rightNodeVariant.isEmpty()) {
+                    Set<StringBuilder> derivationToTermsString = buildShortestDerivationToTermsString(middleNode.get(i));
+                    if (derivationToTermsString != null) {
+                        words.addAll(derivationToTermsString);
+                    }
+                    endedDerivation.add(i);
+                } else {
+                    String expr = rightNodeVariant.get(0);
+                    if (expr.matches(RuleRightSide.NONTERM_REGEX)) {
+                        if (stackNontermsTrace.contains(expr)) {
+                            circledDerivation.add(0, i);
+                        } else if (!rootNontermFound && expr.equals(rootNonterm)) {
+                            setRootNontermFound(i, endedDerivation);
+                            middleNode.get(i).add(new TreeNode(rootNonterm, true));
+                        } else {
+                            Set<String> newStackNontermsTrace = new HashSet<>(stackNontermsTrace);
+                            newStackNontermsTrace.add(expr);
+                            TreeNode newNode = new TreeNode(expr, rules, rootNonterm, rootNontermFound, newStackNontermsTrace);
+                            if (newNode.middleNode.isEmpty()) {
+                                circledDerivation.add(0, i);
+                            } else {
+                                if (newNode.rootNontermFound) {
+                                    setRootNontermFound(i, endedDerivation);
+                                }
+                                middleNode.get(i).add(newNode);
+                            }
+                        }
+                    } else {
+                        middleNode.get(i).add(new TreeNode(expr, false));
+                    }
+                    rightNodeVariant.remove(0);
+                }
+            }
+        }
+        for (int noRootNontermFoundIndex : circledDerivation) {
+            if (rootNontermFound && noRootNontermFoundIndex < indexOfRootNontermInMiddleNodeList) {
+                indexOfRootNontermInMiddleNodeList--;
+            }
+            middleNode.remove(noRootNontermFoundIndex);
+            rightNode.remove(noRootNontermFoundIndex);
+        }
     }
 
     private Set<StringBuilder> buildShortestDerivationToTermsString(List<TreeNode> leftmostDerivationTree) {
@@ -79,5 +122,51 @@ public class TreeNode {
             }
         }
         return words;
+    }
+
+    private void setRootNontermFound(int i, Set<Integer> endedDerivation) {
+        rootNontermFound = true;
+        indexOfRootNontermInMiddleNodeList = i;
+        endedDerivation.add(i);
+    }
+
+    protected boolean checkRootNontermFound() {
+        return rootNontermFound;
+    }
+
+
+    protected String buildLeftMostDerivationToRootNonterm(String rootNonterm) {
+        if (isNonterm) {
+            if (isEndOfDerivation) {
+                return "";
+            }
+            StringBuilder prefix = new StringBuilder();
+            if (rootNontermFound) {
+                for (TreeNode prefixNode : middleNode.get(indexOfRootNontermInMiddleNodeList)) {
+                    prefix.append(prefixNode.buildLeftMostDerivationToRootNonterm(rootNonterm));
+                }
+                List<TreeNode> derivation = middleNode.get(indexOfRootNontermInMiddleNodeList);
+                if (derivation.get(derivation.size() - 1).isEndOfDerivation) {
+                    nontermsAndTermsSuffixOfRootNontermDerivationTree = new ArrayList<>(rightNode.get(indexOfRootNontermInMiddleNodeList));
+                } else {
+                    nontermsAndTermsSuffixOfRootNontermDerivationTree = new ArrayList<>(derivation.get(derivation.size() - 1).nontermsAndTermsSuffixOfRootNontermDerivationTree);
+                    nontermsAndTermsSuffixOfRootNontermDerivationTree.addAll(rightNode.get(indexOfRootNontermInMiddleNodeList));
+                }
+            } else {
+                for (TreeNode prefixNode : middleNode.get(0)) {
+                    prefix.append(prefixNode.buildLeftMostDerivationToRootNonterm(rootNonterm));
+                }
+            }
+            return prefix.toString();
+        }
+        return nodeExpr;
+    }
+
+    protected List<String> getNontermsAndTermsSuffixOfRootNontermDerivationTree() {
+        return nontermsAndTermsSuffixOfRootNontermDerivationTree;
+    }
+
+    protected Set<StringBuilder> getWords() {
+        return this.words;
     }
 }
